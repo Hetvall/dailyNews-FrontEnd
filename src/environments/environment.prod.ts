9db16301c8b07c27db22e3fca8aae30a7965ff58
James Orozco <jamesorozcoh@gmail.com>
2025-09-18 21:06:10 -0500

    Update environment configuration and enhance Vercel settings for production deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  news_apiKey: '',
-  apiUrl: '',
+  news_apiKey: 'd107cfc2d58f46f49bd9bb52ddc96cf9',
+  apiUrl: 'https://dailynews-71ep.onrender.com',
 };
