87a6892c56b868f1260e3c74be5c96c5226ae302
James Orozco <jamesorozcoh@gmail.com>
2025-09-19 19:08:25 -0500

    Update API URL in DashboardService and remove Vercel configuration

--- src/app/dashboard/service/dashboard.service.ts
+++ src/app/dashboard/service/dashboard.service.ts
@@ -6,7 +6,7 @@ import { NewsPageService } from './newsPage.service';
 import { DashboardInterface } from '../interfaces/dashboard.interface';
 import { environment } from '../../../environments/environment';
 
-const apiUrl = 'https://newsapi.org/v2/everything';
+const apiUrl = 'https://dailynews-71ep.onrender.com/api/news';
 
 @Injectable({
   providedIn: 'root',
@@ -16,14 +16,8 @@ export class DashboardService {
   private newsPageService = inject(NewsPageService);
 
   getNews(): Observable<DashboardInterface> {
-    const params = {
-      q: 'news',
-      sortBy: 'publishedAt',
-      apiKey: environment.news_apiKey,
-      pageSize: '11',
-    };
     return this.http
-      .get<DashboardInterface>(apiUrl, { params })
+      .get<DashboardInterface>(apiUrl)
       .pipe(tap((data) => this.newsPageService.setArticles(data.articles)));
   }
 }
